925a09b119c64da02921000cdedf89443592898d
Sparky Rhode <sparkyr@google.com>
2012-08-15 07:22:27 +0100

    Save preference that records last selected item

---
+++ Unit4_Data_Retrieval/Part_2_Final/src/com/example/acamp/dip/PicDetailFragment.java
@@ -0,0 +1,65 @@
+package com.example.acamp.dip;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.acamp.dip.dummy.DummyContent;
+
+public class PicDetailFragment extends Fragment {
+
+    public static final String ARG_ITEM_ID = "item_id";
+
+    DummyContent.DummyItem mItem;
+
+    public PicDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        } else {
+            SharedPreferences sp =
+                    getActivity().getSharedPreferences(DummyContent.PREFS_GROUP,
+                            Context.MODE_PRIVATE);
+            String lastItemKey = sp.getString(DummyContent.LAST_ITEM_KEY, "");
+            if (!TextUtils.isEmpty(lastItemKey)) {
+                mItem = DummyContent.ITEM_MAP.get(lastItemKey);
+            }
+        }
+    }
+
+    @Override
+    public View
+            onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_pic_detail, container, false);
+        if (mItem != null) {
+
+            // The documentation implies that ImageView.setImageDrawable
+            // does not run on the UI thread, so we don't need to do this in
+            // an AsyncTask.
+            AssetManager am = getActivity().getAssets();
+            try {
+                Drawable d = Drawable.createFromStream(am.open(mItem.content), null);
+                ((ImageView) rootView.findViewById(R.id.pic_detail_pic)).setImageDrawable(d);
+            } catch (IOException e) {
+                ((TextView) rootView.findViewById(R.id.pic_detail))
+                        .setText("Could not load asset: " + mItem.content);
+            }
+        }
+        return rootView;
+    }
+}
